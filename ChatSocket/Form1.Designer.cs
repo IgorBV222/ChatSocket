f9578d472f032d1ee33e6b8e68b7bccddf02263a
Suslov Igor <susloviv@mail.ru>
2024-01-28 21:01:26 +0500

    Добавьте файлы проекта.

---
+++ ChatSocket/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ChatSocket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PortText = new System.Windows.Forms.NumericUpDown();
+            this.IPText = new System.Windows.Forms.MaskedTextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbAllMsg = new System.Windows.Forms.TextBox();
+            this.tbMsgText = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PortText)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PortText
+            // 
+            this.PortText.Location = new System.Drawing.Point(145, 27);
+            this.PortText.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.PortText.Name = "PortText";
+            this.PortText.Size = new System.Drawing.Size(68, 20);
+            this.PortText.TabIndex = 1;
+            this.PortText.Value = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            // 
+            // IPText
+            // 
+            this.IPText.Culture = new System.Globalization.CultureInfo("en-GB");
+            this.IPText.Location = new System.Drawing.Point(13, 27);
+            this.IPText.Mask = "###.###.###.###";
+            this.IPText.Name = "IPText";
+            this.IPText.Size = new System.Drawing.Size(107, 20);
+            this.IPText.TabIndex = 2;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(236, 27);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(92, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Подключить";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // tbAllMsg
+            // 
+            this.tbAllMsg.Location = new System.Drawing.Point(13, 97);
+            this.tbAllMsg.Multiline = true;
+            this.tbAllMsg.Name = "tbAllMsg";
+            this.tbAllMsg.Size = new System.Drawing.Size(408, 420);
+            this.tbAllMsg.TabIndex = 4;
+            // 
+            // tbMsgText
+            // 
+            this.tbMsgText.Location = new System.Drawing.Point(13, 539);
+            this.tbMsgText.Name = "tbMsgText";
+            this.tbMsgText.Size = new System.Drawing.Size(408, 20);
+            this.tbMsgText.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(454, 539);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 638);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbMsgText);
+            this.Controls.Add(this.tbAllMsg);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.IPText);
+            this.Controls.Add(this.PortText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PortText)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown PortText;
+        private System.Windows.Forms.MaskedTextBox IPText;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbAllMsg;
+        private System.Windows.Forms.TextBox tbMsgText;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ ChatSocket/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatSocket
+{
+    public partial class Form1 : Form
+    {
+        public Socket socket;
+        public Socket ns;
+        public IPEndPoint ep;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void MsgRecive(IAsyncResult ar) //колбек-функция
+        {
+            socket = (Socket)ar.AsyncState; //получаем ссылку на слушающий сокет            
+            ns = socket.EndAccept(ar); //получаем сокет для обмена данными с клиентом            
+            byte[] buf = new byte[1024]; //создаем буфер
+            int l = ns.Receive(buf); //создаем буфер и записываем в буфер сообщение от клиента
+            Invoke((MethodInvoker)delegate
+            {
+                tbAllMsg.Text += System.Text.Encoding.Unicode.GetString(buf, 0, l) + "\r\n";
+            });
+            ns.Send(buf, 0, buf.Length, SocketFlags.None); // отправляем данные в подключенный Socket            
+            socket.BeginAccept(new AsyncCallback(MsgRecive), socket); //возобновляем асинхронное подключение
+            ns.Shutdown(SocketShutdown.Both); //блокируем передачу и получение данных
+            ns.Close(); //закрываем подключение
+        }
+        
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);  //создаем новый сокет
+            socket.Bind(new IPEndPoint(IPAddress.Any, (int)PortText.Value)); //наш сервер, к нему будут подклучатся
+            socket.Listen(10); //слушаем 10 входящих
+            socket.BeginAccept(new AsyncCallback(MsgRecive), socket); //начинаем асинхронное прослушивание
+            btnConnect.BackColor = Color.Green; //при подключении кнопка меняе цвет
+            btnConnect.Enabled = false; // при подключении кнопка блокируется
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Socket ss = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP); //создаем новый сокет
+            ep = new IPEndPoint(IPAddress.Parse(IPText.Text), (int)PortText.Value); //создаем новый  endPoint с ip адрессом к которому хотим подключиться
+            if (!ss.Connected)
+            {
+                ss.Connect(ep);
+            }
+            ss.Send(System.Text.Encoding.Unicode.GetBytes(tbMsgText.Text)); //отправляем сообщение клиенту
+            tbAllMsg.Text += "My messange => " + tbMsgText.Text + "\r\n"; //выводим наше сообщение в textbox
+            tbMsgText.Clear(); // очистить поле ввода сообщений
+            ss.Shutdown(SocketShutdown.Both);  //блокируем передачу и получение данных
+            ss.Close();  //закрываем подключение
+        }
+    }
+}
